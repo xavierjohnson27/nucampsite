0b3ef8b405a8383125d73921fa8b17d240de098d
Xavier J <xmj359@gmail.com>
2021-12-26 13:13:32 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import {CAMPSITES} from './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
+   
     render() {
         return (
-            <div className="App">
-                <Main />
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>
         );
     }
 }
